ff20b681408a2e9020a7113ba666481064fa99ed
BrazlyBearWolf <merive33@gmail.com>
2025-02-04 18:17:51 +0000

    added about route

---
+++ app/about.tsx
@@ -0,0 +1,25 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function AboutSCreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About Screen</Text>
+    </View>
+
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  text: {
+    color: '#fff'
+  }
+})
---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router"
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Homescreen!</Text>
+      <Link href="/about" style={styles.button}>Go to About Page</Link>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#26293e',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  text: {
+    color: '#fff',
+    fontSize: 70,
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+})
